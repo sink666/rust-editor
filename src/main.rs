05d92a086d7386b3522828fcf52da7648091a048
sink666 <er.basilisk@gmail.com>
2022-01-23 22:42:54 +0000

    comment stuff out

--- src/main.rs
+++ src/main.rs
@@ -300,8 +300,8 @@ fn set_addresses(address_vec: Vec<String>,
     state.address2 = temp_addr2;
     state.dot = temp_dot;
 
-    println!("current; a1: {}, a2: {}, dot: {}",
-             state.address1, state.address2, state.dot);
+    // println!("current; a1: {}, a2: {}, dot: {}",
+    //          state.address1, state.address2, state.dot);
     Ok(num_addrs)
 }
 
